06c9dcf99ed638307b3667ff0689840144c5eea5
mashhadmonitoring <mashhad.monitoring1@gmail.com>
2025-12-19 20:31:09 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets to work on GitHub Pages subfolders
+  base: "https://mashhadmonitoring.github.io/sa/"
   define: {
     // This allows process.env.API_KEY to be available in the browser after build.
     // Ensure the API_KEY environment variable is set during the build process (e.g. in GitHub Actions secrets).
